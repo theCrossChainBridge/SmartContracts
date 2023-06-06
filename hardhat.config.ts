dee564d17d1a587e29877bd997a7f6d158ae86b3
confucian <confucian.e@gmail.com>
2023-06-06 16:52:26 +0800

    add networks support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.20",
+
+  networks: {
+    polygonMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.Mumbai_RPC_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.Sepolia_RPC_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.EtherScan_API_KEY!,
+      polygonMumbai: process.env.PolygonScan_API_KEY!,
+    }
+  }
 };
 
 export default config;
