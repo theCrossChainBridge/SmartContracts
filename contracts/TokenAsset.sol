9c12ecabf8addce2d0a447844be0481304e3e90c
confucian <confucian.e@gmail.com>
2023-06-06 19:19:49 +0800

    add token contract

---
+++ contracts/TokenAsset.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenAsset is ERC20, Ownable {
+    constructor() ERC20("Token Asset", "TA") {
+        _mint(msg.sender, 1e22);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
