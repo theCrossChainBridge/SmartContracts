fd1cdb968b7ceabd5b7e0bcdd26d736793b224fd
Kepler <103641474+hljie@users.noreply.github.com>
2023-06-08 22:53:12 +0800

    Update Bridge.sol

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -8,8 +8,17 @@ import "./interfaces/IBridge.sol";
 contract Bridge is IBridge, Ownable {
     error InsufficientBalance();
 
-    /// account => token_address => balance
-    mapping (address => mapping (address => uint256)) public tokenBalanceOf;
+    uint256 reserveA; // reserve of tokenA
+    uint256 reserveB; // reserve of tokenB
+
+    address public TokenA_addr;
+    address public TokenB_addr;
+
+    constructor (address _TokenA, address _TokenB) {
+        TokenA_addr = _TokenA;
+        TokenB_addr = _TokenB;
+    }
+
 
     /**
      * @dev stake token into contract
@@ -17,14 +26,21 @@ contract Bridge is IBridge, Ownable {
      * @param amount the amount of ERC20 token to stake
      */
     function stake(address token_addr, uint256 amount) external override returns (bool success) {
-        IERC20(token_addr).transferFrom(msg.sender, address(this), amount);
-        unchecked {
-            tokenBalanceOf[msg.sender][token_addr] += amount;   
+
+        uint256 outputAmount = getAmountOut(amount, token_addr);
+        if (token_addr == TokenA_addr) {
+            IERC20(TokenA_addr).transferFrom(msg.sender, address(this), amount);
+            reserveB -= outputAmount;
+        } else if (token_addr == TokenB_addr) {
+            IERC20(TokenB_addr).transferFrom(msg.sender, address(this), amount);
+            reserveA -= outputAmount;
+        } else {
+            revert("error address");
         }
 
         success = true;
 
-        emit Stake(msg.sender, token_addr, amount);
+        emit Stake(msg.sender, token_addr, outputAmount);
     }
 
     /**
@@ -32,12 +48,21 @@ contract Bridge is IBridge, Ownable {
      * @param token_addr the address of ERC20 token
      * @param amount the amount of ERC20 to transfer
      */
-    function mint(address account, address token_addr, uint256 amount) external override onlyOwner returns (bool success) {
-        IERC20(token_addr).transfer(account, amount);
+    function mint(address token_addr, uint256 outputAmount) external override onlyOwner returns (bool success) {
+        uint256 amount = getAmountIn(outputAmount, token_addr);
+        if (token_addr == TokenA_addr) {
+            IERC20(TokenB_addr).transfer(msg.sender, outputAmount);
+            reserveA += amount;
+        } else if(token_addr == TokenB_addr) {
+            IERC20(TokenA_addr).transfer(msg.sender, outputAmount);
+            reserveB += amount;
+        } else {
+            revert("error address");
+        }
 
         success = true;
 
-        emit Mint(account, token_addr, amount);
+        emit Mint(msg.sender, token_addr, amount);
     }
 
     /**
@@ -47,4 +72,34 @@ contract Bridge is IBridge, Ownable {
     function balanceOfToken(address token_addr) view public override returns (uint256 balance) {
         return IERC20(token_addr).balanceOf(address(this));
     }
+
+    // get AmountOut by inputAmount
+    function getAmountOut(uint256 inputAmount, address inputToken) public view returns (uint256 outputAmount) {
+        if (inputToken == TokenA_addr) {
+            outputAmount = (inputAmount * reserveB) / (reserveA + inputAmount);
+        } else if (inputToken == TokenB_addr) {
+            outputAmount = (inputAmount * reserveA) / (reserveB + inputAmount);
+        } else {
+            revert("address error");
+        }
+        
+        return outputAmount;
+
+    }
+
+    // get inputAmount by outputAmount
+    function getAmountIn(uint256 outputAmount, address outputToken) public view returns (uint256 inputAmount) {
+        if (outputToken == TokenA_addr) {
+            inputAmount = (reserveB * outputAmount) / (reserveA - outputAmount);
+        } else if (outputToken == TokenB_addr) {
+            inputAmount = (reserveA * outputAmount) / (reserveA - outputAmount);
+        } else {
+            revert("address error");
+        }
+
+        return inputAmount;
+    }
+
+
+    
 }
